29dff6153eeb450291bf49df05c8f9c2fcf8468c
Alex Clapperton <Pushedskydiver@users.noreply.github.com>
2015-11-11 20:49:52 +0000

    Sorted all files to work with Gulp. Added new assets folder and changed css and js links in html pages to point to assets folder.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,61 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    watch = require('gulp-watch'),
+    del = require('del');
 
-gulp.task('default', function() {
-  // place code here
+// Styles
+gulp.task('styles', function() {
+  return gulp.src('src/css/*.css')
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('assets/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// Images
+gulp.task('images', function() {
+  return gulp.src('src/images/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('assets/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+// Clean
+gulp.task('clean', function() {
+    return del(['assets/css', 'assets/images', 'assets/js']);
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('src/style/*.css', ['minify-css']);
+
+  // Watch .js files
+  gulp.watch('src/js/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('src/images/*', ['images']);
+
 });
