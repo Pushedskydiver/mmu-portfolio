3d95735c1d212a9d912b65b63f693a89d6472b39
Alex Clapperton <Pushedskydiver@users.noreply.github.com>
2015-12-02 21:15:23 +0000

    animsition

--- src/js/script.js
+++ src/js/script.js
@@ -220,4 +220,34 @@ $(function () {
 
 
 
+
+
+//------------------------------------- START: Animsition setup ------------------------------------------------//
+
+$(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 1200,
+    outDuration: 800,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
+    loading: !0,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    unSupportCss: [
+      'animation-duration',
+      '-webkit-animation-duration',
+      '-o-animation-duration'
+    ],
+    //"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    //The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body'
+  });
+
+//------------------------------------- END: Animsition setup ------------------------------------------------//
+
+
+
 })(jQuery);
